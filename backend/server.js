5a840cdf3a184e758ee1d91e3e677e551160ab1f
Danil <NpDen4ik@yandex.ru>
2019-10-29 12:06:33 +0600

    first course is finish

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ let Todo = require('./todo.model');
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true});
+mongoose.connect('mongodb://127.0.0.1:27017', {useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function() {
@@ -20,13 +20,13 @@ connection.once('open', function() {
 });
 
 todoRoutes.route('/').get(function(req, res){
-    Todo.find(function(err, todos)){
+    Todo.find(function(err, todos){
         if(err){
             console.log(err);
         } else {
             res.json(todos);
         }
-    }
+    })
 });
 
 todoRoutes.route('/:id').get(function(req, res) {
@@ -43,8 +43,29 @@ todoRoutes.route('/add').post(function(req, res){
             res.status(200).json({'todo': 'todo added successfully'});
         })
         .catch(err => {
-            res.status(400).send('adding new todo failed');
-        })
+            res.status(400).send('adding new todo failed'); 
+        });
+});
+
+todoRoutes.route('/update/:id').post(function(req, res) {
+    Todo.findById(req.params.id, function(err, todo) {
+        if(!todo)
+            res.status(404).send('data is not found');
+        else
+            todo.todo_description  = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+
+            todo.save().then(todo => {
+                res.json('Todo updated');
+            })
+            .catch(err=> {
+                res.status(400).send("update not possible");
+            });
+
+    });
 });
 
 app.use('/todos', todoRoutes);
